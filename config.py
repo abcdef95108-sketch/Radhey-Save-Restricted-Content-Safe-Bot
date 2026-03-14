6130146be20af107d04af462a5b0194bc514c2d9
abcdef95108-sketch <abcdef95108@gmail.com>
2026-03-14 10:44:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # safe_repo
 # Note if you are trying to deploy on vps then directly fill values in ("")
 
-API_HASH = "f150646c78f09b4f88bef191a22539c0"
-API_ID = 26704085
-BOT_TOKEN = "8564747078:AAF39Ekn22SZxQB7ShELURFel981IFhrmoM"
-CHANNEL_ID = -1003818713803
-LOG_GROUP = -1005121908460
+API_HASH = "4d908be0390fb1f4fd079506af5b1971"
+API_ID = "30978477"
+BOT_TOKEN = ""
+CHANNEL_ID = -1003600843995
+LOG_GROUP = -1003600843995
 # Owner(s) of the bot. Keep as a list for filters.user compatibility.
-OWNER_ID = [8552899459]
+OWNER_ID = [5575032909]
 
 # MongoDB removed from this codebase; keep MONGO_DB for backwards compatibility
 MONGO_DB = ""
